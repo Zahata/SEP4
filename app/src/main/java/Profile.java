dd4901c7c993680cb2b16208e2f833713d742317
Zahata <37177462+Zahata@users.noreply.github.com>
2018-12-03 11:39:28 +0100

    added Profile class and SetupProfile Activity

---
+++ app/src/main/java/Profile.java
@@ -0,0 +1,59 @@
+public class Profile {
+
+    public String photoURL;
+    public String fullName;
+    public String address;
+    public String studyProgramme;
+    public String interests;
+
+
+    public Profile(){}
+
+    public Profile(String photo, String name, String address, String programme, String interests){
+        this.photoURL = photo;
+        this.fullName = name;
+        this.address = address;
+        this.studyProgramme = programme;
+        this.interests = interests;
+    }
+
+    public String getPhotoURL() {
+        return photoURL;
+    }
+
+    public void setPhotoURL(String photoURL) {
+        this.photoURL = photoURL;
+    }
+
+    public String getFullName() {
+        return fullName;
+    }
+
+    public void setFullName(String fullName) {
+        this.fullName = fullName;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getStudyProgramme() {
+        return studyProgramme;
+    }
+
+    public void setStudyProgramme(String studyProgramme) {
+        this.studyProgramme = studyProgramme;
+    }
+
+    public String getInterests() {
+        return interests;
+    }
+
+    public void setInterests(String interests) {
+        this.interests = interests;
+    }
+}
---
+++ app/src/main/java/com/sep/viasocial/SetupProfile.java
@@ -0,0 +1,15 @@
+package com.sep.viasocial;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class SetupProfile extends AppCompatActivity {
+
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_setup_profile);
+    }
+}
