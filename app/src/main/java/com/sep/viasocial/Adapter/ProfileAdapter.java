6b0388d10bf400a9033a61e27dd5642b3ad3ad59
Q-and <43401968+q-and@users.noreply.github.com>
2018-12-05 19:32:52 +0100

    users fragment

---
+++ app/src/main/java/com/sep/viasocial/Adapter/ProfileAdapter.java
@@ -0,0 +1,103 @@
+package com.sep.viasocial.Adapter;
+
+import android.content.Context;
+import android.content.Intent;
+import android.support.annotation.NonNull;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.bumptech.glide.Glide;
+import com.sep.viasocial.Profile;
+import com.sep.viasocial.R;
+
+import java.util.List;
+
+public class ProfileAdapter extends RecyclerView.Adapter<ProfileAdapter.ViewHolder> {
+
+    private Context mContext;
+    private List<Profile> mProfiles;
+    // private boolean ischat;
+
+    //String theLastMessage;
+
+    public ProfileAdapter(Context mContext, List<Profile> mProfiles){ //, boolean ischat){
+        this.mProfiles = mProfiles;
+        this.mContext = mContext;
+        //this.ischat = ischat;
+    }
+
+    @NonNull
+    @Override
+    public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(mContext).inflate(R.layout.user_item, parent, false);
+        return new ProfileAdapter.ViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
+
+        Profile profile = mProfiles.get(position);
+        holder.username.setText(profile.getFullName());
+        if (profile.getPhotoURL().equals("default")){ //change to empty
+            holder.profile_image.setImageResource(R.mipmap.ic_launcher);
+        } else {
+            Glide.with(mContext).load(profile.getPhotoURL()).into(holder.profile_image);
+        }
+
+        /*if (ischat){
+            lastMessage(user.getId(), holder.last_msg);
+        } else {
+            holder.last_msg.setVisibility(View.GONE);
+        }
+
+        if (ischat){
+            if (user.getStatus().equals("online")){
+                holder.img_on.setVisibility(View.VISIBLE);
+                holder.img_off.setVisibility(View.GONE);
+            } else {
+                holder.img_on.setVisibility(View.GONE);
+                holder.img_off.setVisibility(View.VISIBLE);
+            }
+        } else {
+            holder.img_on.setVisibility(View.GONE);
+            holder.img_off.setVisibility(View.GONE);
+        }*/
+
+        /*holder.itemView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(mContext, MessageActivity.class);
+                intent.putExtra("userid", user.getId());
+                mContext.startActivity(intent);
+            }
+        });*/
+    }
+
+    @Override
+    public int getItemCount() {
+        return mProfiles.size();
+    }
+
+    public  class ViewHolder extends RecyclerView.ViewHolder{
+
+        public TextView username;
+        public ImageView profile_image;
+        //private ImageView img_on;
+        //private ImageView img_off;
+        //private TextView last_msg;
+
+        public ViewHolder(View itemView) {
+            super(itemView);
+
+            username = itemView.findViewById(R.id.username);
+            profile_image = itemView.findViewById(R.id.profile_image);
+            //img_on = itemView.findViewById(R.id.img_on);
+            //img_off = itemView.findViewById(R.id.img_off);
+            //last_msg = itemView.findViewById(R.id.last_msg);
+        }
+    }
+}
--- app/src/main/java/com/sep/viasocial/FriendsFragment.java
+++ app/src/main/java/com/sep/viasocial/FriendsFragment.java
@@ -2,22 +2,79 @@ package com.sep.viasocial;
 
 
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.v4.app.Fragment;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+import com.sep.viasocial.Adapter.ProfileAdapter;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class FriendsFragment extends Fragment {
 
-    public FriendsFragment() {
-        // Required empty public constructor
-    }
+    //private Toolbar mToolbar;
+    private RecyclerView mUsersList;
+
+    private ProfileAdapter profileAdapter;
+    private List<Profile> mProfiles;
 
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_friends, container, false);
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        super.onCreateView(inflater, container, savedInstanceState);
+        View view = inflater.inflate(R.layout.fragment_friends, container, false);
+
+        mUsersList = view.findViewById(R.id.users_list);
+        mUsersList.setHasFixedSize(true);
+        mUsersList.setLayoutManager(new LinearLayoutManager(getContext()));
+
+        mProfiles = new ArrayList<>();
+
+        readProfiles();
+
+        return view;
     }
 
+    private void readProfiles() {
+        final FirebaseUser firebaseUser = FirebaseAuth.getInstance().getCurrentUser();
+        DatabaseReference reference = FirebaseDatabase.getInstance().getReference("Users");
+
+        reference.addValueEventListener(new ValueEventListener() {
+
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                mProfiles.clear();
+
+                //if (search_users.getText().toString().equals("")) {
+                //mUsers.clear();
+                for (DataSnapshot snapshot : dataSnapshot.getChildren()) {
+                    if (!snapshot.getKey().equals(firebaseUser.getUid())) {
+                        Profile profile = snapshot.getValue(Profile.class);
+                        mProfiles.add(profile);
+                    }
+                }
+
+                profileAdapter = new ProfileAdapter(getContext(), mProfiles);
+                mUsersList.setAdapter(profileAdapter);
+                //}
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+
+            }
+        });
+    }
 }
